0719d82624274fdbbf6d6b1a005ac6cfbe656b6b
Vasyl <108887823+VasylMolnar@users.noreply.github.com>
2026-05-26 09:57:07 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import categoriesRoutes from "./routes/categories.routes";
 import "./db";
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,6 @@ app.use(express.json());
 app.use("/api/todos", todoRoutes);
 app.use("/api/categories", categoriesRoutes);
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("Server running on http://localhost:3001");
 });
